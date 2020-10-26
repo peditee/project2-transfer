6a9d51cdbc3e5b869d1c6b7eb9982dfd577208c7
Peter <peditee@gmail.com>
2020-10-26 18:34:30 +1100

    changed_also_reload

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'pry' if development?
-also_reload 'db/data_access'
+also_reload 'db/data_access' if development?  
 require 'pg'
 require 'bcrypt'
 
